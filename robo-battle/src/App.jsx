cfa2255d2514c10573a0c1614170fd86bf38be83
Udev2002 <udayanga.dev.lk@gmail.com>
2024-05-18 11:46:36 -0700

    create project

---
+++ robo-battle/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <h1 className="mb-10 pt-10 justify-center items-center text-3xl ml-10 font-bold underline">Hello maneesha and sithum i have react vite templte and tailwind css !</h1>;
+};
+
+export default App;
