acb17e401e38fd1aee3fe6e3c6affbc1d9039114
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2024-05-24 23:11:32 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Countdown from "./components/countdown";
 import Timeline from "./components/timeline"
 import Navbar from "./components/Navbar/Navbar";
-
+import Home from "./components/Home/Home";
 
 const App = () => {
   return (
-    <div className="bg-purple-800 min-h-screen">
+    <div className="min-h-screen bg-purple-800">
       <Navbar />
+      <Home/>
       <Countdown />
       <Timeline />
     </div>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+  <>
+    <h1>Creative Robotic</h1>
+    
+  </>
+  )
+}
+
+export default Home
