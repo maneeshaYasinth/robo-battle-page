71743fe0185d5cd9720e848bdc043a274768025f
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2024-05-26 02:28:35 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,11 +6,11 @@ function Home() {
   return (
   <>
    <div className='max-w-xl ml-20 text-white'>
-    <div className='text-xl leading-normal tracking-widest text-left uppercase font-krona mt-14 mt-30'>Creative robotics</div>
-    <div className='text-6xl leading-normal tracking-widest text-left text-white uppercase font-krona'>UOK ROBOT BATTLE</div>
-    <div className='text-6xl leading-normal tracking-widest text-left text-white uppercase font-krona'>2K24</div>
+    <div className='text-xl leading-normal tracking-widest text-left uppercase max-w-screen font-krona mt-14 mt-30'>Creative robotics</div> 
+    <div className='text-6xl leading-normal tracking-widest text-left text-white uppercase max-w-screen font-krona'>UOK ROBOT BATTLES</div>
+    <div className='text-6xl leading-normal tracking-widest text-left text-white uppercase max-w-screen font-krona'>2K24</div>
     <div>
-        <p className='font-semibold leading-normal tracking-widest text-left text-white'>
+        <p className='leading-normal tracking-widest text-left text-white font-poppins'>
         Welcome to the ultimate showdown of engineering prowess and strategy. Here, brilliant minds converge to pit their robotic creations against one another in a 
         thrilling display of technological might. Join us in the arena where the future of robotics comes alive.
         </p>
@@ -20,7 +20,7 @@ function Home() {
         <a href="#" >Register</a></button> 
     </div>
     <div className='bottom-0 w-full top-12 xl:w-1/2 xl:absolute right-1'>
-        <img src="1.png" alt="" />
+        <img src="1.png" alt="1" />
     </div>
 
    </div>
