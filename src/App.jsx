ef6a0018856dfdd6c0e09ee6cc19e6c6184397a5
Sithum39 <115446245+Sithum39@users.noreply.github.com>
2024-05-26 01:27:07 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Countdown from "./components/countdown";
 import Timeline from "./components/timeline"
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import Button from "./components/Button/Button";
+
 
 const App = () => {
   return (
     <div className="min-h-screen bg-purple-800">
-      <Navbar />
+      <Navbar/>
       <Home/>
-      <Button/>
+     
       <Countdown />
       <Timeline />
     </div>
