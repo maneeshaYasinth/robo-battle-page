fc24f6b95c5280e524cac1ce121fb42624d070e2
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-26 19:07:21 +0530

    romove the disturbing white line

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Countdown from "./components/countdown";
 import Timeline from "./components/timeline";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-
 import Registration from "./components/Registration/Registration";
 import Awareness from "./components/AwarenessSession/Awareness";
 import ShinyEffect from "./components/ShinyEffect/ShinyEffect";
@@ -13,23 +12,20 @@ import WininngPrices from "./components/WinningPrices/WininngPrices";
 
 const App = () => {
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen relative overflow-hidden"> {/* Ensure relative positioning and no overflow */}
       <Navbar />
 
-      <div className=" absolute inset-0 overflow-hidden md:overflow-visible">
+      <div className="absolute inset-0">
         <ShinyEffect left={1000} top={0} size={1000} />
         <div className="hidden md:block">
           <ShinyEffect left={0} top={700} size={1200} />
         </div>
-        
       </div>
       <Home />
-
       <Registration />
-
       <Countdown />
       <Awareness />
-<WininngPrices/>
+      <WininngPrices />
       <Timeline />
       <Team />
       {/* <Footer/> */}
