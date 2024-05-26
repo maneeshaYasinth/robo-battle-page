a34fd11985e3a72162d2121d5de9434f08b05ae7
Udev2002 <udayanga.dev.lk@gmail.com>
2024-05-26 00:28:53 -0700

    add pink effect

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ import Home from "./components/Home/Home";
 
 import Registration from "./components/Registration/Registration";
 import Awareness from "./components/AwarenessSession/Awareness";
+import ShinyEffect from "./components/ShinyEffect/ShinyEffect";
 
 const App = () => {
   return (
     <div className="min-h-screen ">
       <Navbar />
+      
+      <div>
+      <ShinyEffect left={700} top={0} size={800} />
+      </div>
       <Home />
+      <div>
+      <ShinyEffect left={0} top={700} size={900} />
+      </div>
       <Registration />
 
       <Countdown />
