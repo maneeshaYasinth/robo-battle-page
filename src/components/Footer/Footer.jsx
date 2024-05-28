bb5a231845643d80f71dfad05eb05def7dba996b
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2024-05-28 16:24:54 +0530

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,10 +10,10 @@ function Footer() {
         <footer className="footer-distributed">
             <div className="grid grid-cols-2 gap-0  font-krona">
                 <div className=' text-white bg-gray-900 p-4 md:px-4'>
-                    <p className='pr-8 pl-24 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
+                    <p className='pr-8 pl-8 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
                 </div>
                 <div className=' text-white bg-gray-900 p-4 md:px-4 '>
-                    <p className='pr-0 pl-20 text-base md:text-3xl lg:ml-40 '>FOLLOW US ON</p>
+                    <p className='pr-0 pl-16 text-base md:text-3xl sm:ml-10 lg:ml-40 xl:ml-60 2xl:ml-80 '>Follow Us On</p>
                 </div>
             </div>
     
@@ -23,7 +23,7 @@ function Footer() {
                         <img src="club_logo.png" alt="1" className='w-4/6 md:w-1/3 ml-12 md:ml-44 mt-4'/>
                     </div>
                     <div className='flex justify-center md:justify-start'>
-                        <h3 className=' -ml-2 xl:-ml-24 mt-8 xl:mt-20 text-white text-lg lg:text-2xl text-center md:text-left '>
+                        <h3 className=' ml-2  xl:-ml-24 mt-8 xl:mt-20 text-white text-lg lg:text-2xl text-center md:text-left '>
                             Electronics and Computer<br/>
                             <span>Science Club</span>
                         </h3>
@@ -36,7 +36,7 @@ function Footer() {
                             <ImLinkedin className='w-10 h-10 md:w-14 md:h-14 lg:mx-4 text-white'/>
                         </a>
                         <a href="https://www.youtube.com/@ECSCUOK">
-                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 lg:-mx-4  text-white'/>
+                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 lg:mx-0 xl:-mx-4  text-white'/>
                         </a>
                     </div>
                 </div>
