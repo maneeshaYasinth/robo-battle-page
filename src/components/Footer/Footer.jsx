5a9bf0d7627950a4587d2f806696bf1b05d06fd6
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2024-05-28 11:02:30 +0530

    footer update 3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ function Footer() {
                     <p className='pr-8 pl-24 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
                 </div>
                 <div className=' text-white bg-gray-900 p-4 md:px-4 '>
-                    <p className='pr-8 pl-24 text-base md:text-3xl md:ml-40 '>FOLLOW US ON</p>
+                    <p className='pr-0 pl-20 text-base md:text-3xl lg:ml-40 '>FOLLOW US ON</p>
                 </div>
             </div>
     
@@ -23,20 +23,20 @@ function Footer() {
                         <img src="club_logo.png" alt="1" className='w-4/6 md:w-1/3 ml-12 md:ml-44 mt-4'/>
                     </div>
                     <div className='flex justify-center md:justify-start'>
-                        <h3 className='-ml-6 md:-ml-24 mt-8 md:mt-20 text-white text-lg md:text-2xl text-center md:text-left'>
+                        <h3 className=' -ml-2 xl:-ml-24 mt-8 xl:mt-20 text-white text-lg lg:text-2xl text-center md:text-left '>
                             Electronics and Computer<br/>
                             <span>Science Club</span>
                         </h3>
                     </div>
-                    <div className='md:grid grid-cols-3 gap-0 mx-12 place-content-start mt-8 md:ml-6 '>
+                    <div className='lg:grid grid-cols-3 gap-0 mx-12 place-content-start mt-8 md:ml-6 '>
                         <a href="https://web.facebook.com/UOKRobotBattles">
-                            <IoLogoFacebook className='w-10 h-10 md:w-16 md:h-16 md:mx-8 text-white'/>
+                            <IoLogoFacebook className='w-10 h-10 md:w-16 md:h-16 lg:mx-8 text-white'/>
                         </a>
                         <a href="https://www.linkedin.com/company/uokrobotbattles/posts/?feedView=all">
-                            <ImLinkedin className='w-10 h-10 md:w-14 md:h-14 md:mx-4 text-white'/>
+                            <ImLinkedin className='w-10 h-10 md:w-14 md:h-14 lg:mx-4 text-white'/>
                         </a>
                         <a href="https://www.youtube.com/@ECSCUOK">
-                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 md:-mx-4  text-white'/>
+                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 lg:-mx-4  text-white'/>
                         </a>
                     </div>
                 </div>
