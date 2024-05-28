714275f86d233c69d98860ae2839b03e78a4826b
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2024-05-28 09:32:14 +0530

    footer update 2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,57 +1,51 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { IoLogoFacebook } from "react-icons/io";
 import { ImLinkedin } from "react-icons/im";
 import { ImYoutube } from "react-icons/im";
 
 function Footer() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <footer className="footer-distributed">
-      <div className="grid grid-cols-2 gap-0 font-krona">
-        <div className=" text-white bg-gray-900 p-4 md:px-4">
-          <p className="md:px-[-0px] px-10 text-xl md:text-3xl md:ml-28 ">Organized By</p>
-        </div>
-        <div className=" text-white bg-gray-900 p-4 md:px-4 ">
-          <p className="px-20 text-xl md:text-3xl md:ml-36 ">FOLLOW US ON</p>
-        </div>
-      </div>
-
-      <div className="footer-left">
-        <div className="grid grid-cols-3 gap-0">
-          <div className="flex justify-center md:justify-start">
-            <img
-              src="club_logo.png"
-              alt="1"
-              className="w-1/2 md:w-[100px] md:h-[100px] ml-1 md:mt-14 md:ml-[150px] mt-4 "
-            />
-          </div>
-          <div className="flex justify-center md:justify-start">
-            <h3 className="-ml-9 md:-ml-24 mt-4 md:mt-20 text-white text-lg md:text-2xl text-center md:text-left">
-              Electronics and Computer
-              <br />
-              <span>Science Club</span>
-            </h3>
-          </div>
-          {/* grid grid-cols-3 gap-3 place-content-start mt-8 mr-5 */}
-          <div className=" flex flex-row gap-5 md:gap-20 mr-10 items-center content-center ">
-            <a href="https://web.facebook.com/UOKRobotBattles">
-              <IoLogoFacebook className="w-10 h-10 md:w-16 md:h-16  text-white/70" />
-            </a>
-            <a href="https://www.linkedin.com/company/uokrobotbattles/posts/?feedView=all">
-              <ImLinkedin className="w-8 h-10 md:w-12 md:h-14  text-white/70" />
-            </a>
-            <a href="https://www.youtube.com/@ECSCUOK">
-              <ImYoutube className="w-10 h-10 md:w-14 md:h-[200px]   text-white/70" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <p className="text-gray-400 text-xs md:text-sm font-normal text-center mb-8 mt-5 md:mb-9">
-        © 2024 All rights reserved
-      </p>
-    </footer>
-  );
+    const [count, setCount] = useState(0);
+      
+    return (
+        <footer className="footer-distributed">
+            <div className="grid grid-cols-2 gap-0  font-krona">
+                <div className=' text-white bg-gray-900 p-4 md:px-4'>
+                    <p className='pr-8 pl-24 text-base text-center md:text-3xl md:ml-8 '>Organized By</p>
+                </div>
+                <div className=' text-white bg-gray-900 p-4 md:px-4 '>
+                    <p className='pr-8 pl-24 text-base md:text-3xl md:ml-40 '>FOLLOW US ON</p>
+                </div>
+            </div>
+    
+            <div className="footer-left">
+                <div className="grid grid-cols-3 gap-0">
+                    <div className='flex justify-center md:justify-start'>
+                        <img src="club_logo.png" alt="1" className='w-4/6 md:w-1/3 ml-12 md:ml-44 mt-4'/>
+                    </div>
+                    <div className='flex justify-center md:justify-start'>
+                        <h3 className='-ml-6 md:-ml-24 mt-8 md:mt-20 text-white text-lg md:text-2xl text-center md:text-left'>
+                            Electronics and Computer<br/>
+                            <span>Science Club</span>
+                        </h3>
+                    </div>
+                    <div className='md:grid grid-cols-3 gap-0 mx-12 place-content-start mt-8 md:ml-6 '>
+                        <a href="https://web.facebook.com/UOKRobotBattles">
+                            <IoLogoFacebook className='w-10 h-10 md:w-16 md:h-16 md:mx-8 text-white'/>
+                        </a>
+                        <a href="https://www.linkedin.com/company/uokrobotbattles/posts/?feedView=all">
+                            <ImLinkedin className='w-10 h-10 md:w-14 md:h-14 md:mx-4 text-white'/>
+                        </a>
+                        <a href="https://www.youtube.com/@ECSCUOK">
+                            <ImYoutube className='w-10 h-10 md:w-14 md:h-14 md:-mx-4  text-white'/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <p className='text-gray-400 text-xs md:text-sm font-normal text-center mb-8 mt-5 md:mb-9'>
+                © 2024 All rights reserved
+            </p>
+        </footer>
+    );
 }
 
 export default Footer;
