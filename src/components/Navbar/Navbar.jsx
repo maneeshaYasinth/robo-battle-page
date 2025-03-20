24d6fdf1d57f5d013ff8010ea530ebb38f400859
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-03-20 22:22:00 +0530

    8.23 P.M. Update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FiMenu, FiX } from "react-icons/fi";
-import logo from "../../assets/logo.svg"; // Adjust the path to your logo
+import logo from "../../assets/Logo.svg"; // Adjust the path to your logo
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,20 +18,57 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 z-50 w-full bg-[#00000066] shadow-[0px_4px_4px_#00000040] backdrop-blur-[50px] backdrop-brightness-[100%] [-webkit-backdrop-filter:blur(50px)_brightness(100%)] font-transrobotics">
       <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-20"> {/* Adjusted height */}
+        <div className="relative flex items-center justify-between h-20">
+          {" "}
+          {/* Adjusted height */}
           {/* Logo */}
           <div className="flex items-center">
             <img src={logo} alt="Logo" className="w-12 h-12" />
           </div>
-
           {/* Nav Links - Desktop */}
           <div className="hidden center sm:flex sm:items-center sm:ml-6 sm:space-x-4">
-            <NavLink to="/#home" activeLink={activeLink} onClick={() => handleLinkClick("/#home")}>Home</NavLink>
-            <NavLink to="/#rules" activeLink={activeLink} onClick={() => handleLinkClick("/#rules")}>Rules</NavLink>
-            <NavLink to="/#about" activeLink={activeLink} onClick={() => handleLinkClick("/#about")}>About</NavLink>
-            <NavLink to="/#timeline" activeLink={activeLink} onClick={() => handleLinkClick("/#timeline")}>Timeline</NavLink>
-            <NavLink to="/#prizes" activeLink={activeLink} onClick={() => handleLinkClick("/#prizes")}>Prizes</NavLink>
-            <NavLink to="/#contact" activeLink={activeLink} onClick={() => handleLinkClick("/#contact")}>Contact</NavLink>
+            <NavLink
+              to="/#home"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#home")}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/#rules"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#rules")}
+            >
+              Rules
+            </NavLink>
+            <NavLink
+              to="/#about"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#about")}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/#timeline"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#timeline")}
+            >
+              Timeline
+            </NavLink>
+            <NavLink
+              to="/#prizes"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#prizes")}
+            >
+              Prizes
+            </NavLink>
+            <NavLink
+              to="/#contact"
+              activeLink={activeLink}
+              onClick={() => handleLinkClick("/#contact")}
+            >
+              Contact
+            </NavLink>
           </div>
           <div className="relative right-0 flex items-center">
             <Link
@@ -41,7 +78,6 @@ const Navbar = () => {
               <div className="text-center  text-xl font-normal">Register</div>
             </Link>
           </div>
-
           {/* Mobile Menu Button */}
           <div className="sm:hidden">
             <button
@@ -74,13 +110,52 @@ const Navbar = () => {
             </div>
 
             {/* Navigation Links */}
-            <div className="flex flex-col items-start space-y-4" onClick={toggleMenu}>
-              <NavLink to="/#home" activeLink={activeLink} onClick={() => handleLinkClick("/#home")}>Home</NavLink>
-              <NavLink to="/#rules" activeLink={activeLink} onClick={() => handleLinkClick("/#rules")}>Rules</NavLink>
-              <NavLink to="/#about" activeLink={activeLink} onClick={() => handleLinkClick("/#about")}>About</NavLink>
-              <NavLink to="/#timeline" activeLink={activeLink} onClick={() => handleLinkClick("/#timeline")}>Timeline</NavLink>
-              <NavLink to="/#prizes" activeLink={activeLink} onClick={() => handleLinkClick("/#prizes")}>Prizes</NavLink>
-              <NavLink to="/#contact" activeLink={activeLink} onClick={() => handleLinkClick("/#contact")}>Contact</NavLink>
+            <div
+              className="flex flex-col items-start space-y-4"
+              onClick={toggleMenu}
+            >
+              <NavLink
+                to="/#home"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#home")}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/#rules"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#rules")}
+              >
+                Rules
+              </NavLink>
+              <NavLink
+                to="/#about"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#about")}
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/#timeline"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#timeline")}
+              >
+                Timeline
+              </NavLink>
+              <NavLink
+                to="/#prizes"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#prizes")}
+              >
+                Prizes
+              </NavLink>
+              <NavLink
+                to="/#contact"
+                activeLink={activeLink}
+                onClick={() => handleLinkClick("/#contact")}
+              >
+                Contact
+              </NavLink>
             </div>
           </div>
         </div>
@@ -114,7 +189,9 @@ const NavLink = ({ to, children, activeLink, onClick }) => {
     <Link
       to={to}
       onClick={handleClick}
-      className={`w-full px-4 py-2 text-xl font-medium text-white rounded-full hover:text-[#23C4FF] ${activeLink === to ? 'text-[#23C4FF]' : ''}`}
+      className={`w-full px-4 py-2 text-xl font-medium text-white rounded-full hover:text-[#23C4FF] ${
+        activeLink === to ? "text-[#23C4FF]" : ""
+      }`}
     >
       {children}
     </Link>
